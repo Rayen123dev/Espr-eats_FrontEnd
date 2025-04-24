89c742c7da19cd7b8fc84d51b072fbce0ed3af6a
mehdi <mehdi.bouadila@esprit.tn>
2025-04-24 21:45:32 +0100

    aaa

--- src/app/models/Produit.model.ts
+++
@@ -1,9 +0,0 @@
-export class Produit {
-    produitID?: number; // Optional (since it is auto-generated)
-    nomProduit!: string;
-    description!: string;
-    quantite!: number;
-    seuil_alerte!: number;
-    date_peremption!: Date;
-    barcode!: string;
-}
